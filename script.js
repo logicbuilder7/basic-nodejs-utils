c7cff928e460c2ff2033e838d422bb35682adbff
Digvej <sdigvej.07@gmail.com>
2026-04-06 12:11:33 +0530

    Initial commit: Basic Node.js Utilities

---
+++ script.js
@@ -0,0 +1,38 @@
+// let n = 5;
+
+//  for( let i = 0; i < n; i++) {
+//     console.log("hello, ", i);
+//  }
+
+
+// console.log(process.argv);
+
+
+// let args = process.argv;
+// for(let i = 2; i<args.length; i++) {
+//    console.log("hello to ", args[i]);
+// }
+
+
+
+
+// //exports in file
+// const math = require("./math");
+// console.log(math.sum(2, 2));
+// console.log(math.PI);
+
+
+
+// const info = require("./Fruits");
+// console.log(info);
+
+
+
+// import {sum, PI} from "./math.js";
+// console.log(sum(1,2));
+// console.log(PI);
+
+
+
+import { generate, } from "random-words";
+console.log(generate());
